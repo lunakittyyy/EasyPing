74aa06946acea4d011aca7fc4c2cff32f3b3b04f
Lillie <lennon.m.craig@gmail.com>
2023-01-03 13:34:10 -0500

    I forgot to update the version number, oops

--- EasyPing/Plugin.cs
+++ EasyPing/Plugin.cs
@@ -3,7 +3,7 @@ using Bepinject;
 
 namespace ComputerModExample
 {
-    [BepInPlugin("org.kaylie.gtag.easyping", "EasyPing", "1.0.0")]
+    [BepInPlugin("org.kaylie.gtag.easyping", "EasyPing", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
